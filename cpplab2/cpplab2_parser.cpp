7437fc4754078a7719fa564c3d0658003b43c8dc
George Lopatenko <lopa10ko239@mail.ru>
2022-03-20 01:43:13 +0300

    add: lab2 sqlite3

--- cpplab2/cpplab2_parser.cpp
+++ cpplab2/cpplab2_parser.cpp
@@ -1,65 +1,32 @@
 #include <iostream>
-#include "sqlite3.h"
+#include <sqlite3.h>
 #define CURL_STATICLIB
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/writer.h"
+#include "include/rapidjson/stringbuffer.h"
 #include <curl/curl.h>
 #include <string>
+#define ERRCHECK {if (err!=NULL) {printf("%s\n",err); sqlite3_free(err);  return 0;}}
 
 static size_t WriteCallback(void *contents, size_t size, size_t npos, void *userp) {
     ((std::string *) userp) -> append((char *) contents, size * npos);
     return size * npos;
 }
 
-//struct connection_details {
-//    const char *host, *user, *passwd, *db, *unix_socket;
-//    unsigned long port, flags;
-//};
-//MYSQL *mysql_connection_setup(struct connection_details info) {
-//    MYSQL *connection = mysql_init(NULL);
-//    if (!mysql_real_connect(connection, info.host, info.user, info.passwd, info.db, info.port = 0, info.unix_socket = NULL, info.flags = 0)) {
-//        std::cout << "Not connected with MySQL! " << mysql_error(connection) << std::endl;
-//        exit(1);
-//    }
-//    return connection;
-//}
-//MYSQL_RES *mysql_perform_query(MYSQL *connection, const char *sql_query) {
-//    if (mysql_query(connection, sql_query)) {throw std::invalid_argument(mysql_error(connection));}
-//    return mysql_use_result(connection);
-//}
-
+int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    // int argc: holds the number of results
+    // (array) azColName: holds each column returned
+    // (array) argv: holds each value
+    for(int i = 0; i < argc; i++) {std::cout << azColName[i] << ": " << argv[i] << std::endl;}
+    std::cout << std::endl;
+    return 0;
+}
 int main() {
-//    MYSQL *con;	// the connection
-//    MYSQL_RES *res;	// the results
-//    MYSQL_ROW row;	// the results rows (array)
-//
-//    struct connection_details mysqlD;
-//    mysqlD.host = "localhost";  // where the mysql database is
-//    mysqlD.user = "username"; // user
-//    mysqlD.passwd = "password"; // the password for the database
-//    mysqlD.db = "mydatabase";	// the databse
-//
-//    // connect to the mysql database
-//    con = mysql_connection_setup(mysqlD);
-//
-//    // get the results from executing commands
-//    res = mysql_perform_query(con, "select * from tblUsers;");
-//
-//    std::cout << ("Database Output:\n") << std::endl;
-
-//    while ((row = mysql_fetch_row(res)) != NULL){
-//        // the below row[] parametes may change depending on the size of the table and your objective
-//        std::cout << row[0] << " | " << row[1] << " | " << row[2] << " | " << row[3] << " | " << row[4] << std::endl << std::endl;
-//    }
-//
-//    // clean up the database result
-//    mysql_free_result(res);
-//
-//    // close database connection
-//    mysql_close(con);
-//    std::cout<<"hrr"<<std::endl;
-
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+    std::string sql_script;
+    char *error_msg = 0;
+    if (int connect = sqlite3_open("currency.db", &db)) {throw std::invalid_argument("ERROR: Database not connected");}
     CURL *curl = curl_easy_init();
     CURLcode curl_status;
     std::string not_parsed_str;
@@ -71,7 +38,6 @@ int main() {
         curl_easy_cleanup(curl);
         not_parsed_str.erase(0, 90);
         not_parsed_str.erase(not_parsed_str.length() - 2, 2);
-        //std::cout << not_parsed_str << std::endl;
         rapidjson::Document doc_json;
         doc_json.Parse(not_parsed_str.c_str());
         assert(doc_json.IsObject());
@@ -89,13 +55,21 @@ int main() {
             const rapidjson::Value& current_value = itr->value;
             std::cout << current_value["Value"].GetDouble() << std::endl;
         }
-        std::cout << date << std::endl;
 
+        sql_script = "INSERT INTO main.currency_list (id, curr_name, median, mean_span, value_1) " \
+                     "VALUES (4, 'eWR', 32, 23, 110);";
+        std::cout << sql_script << std::endl;
+        sqlite3_exec(db, sql_script.c_str(), callback, 0, reinterpret_cast<char **>(error_msg));
+        sql_script = "SELECT * FROM 'main.currency_list';";
+        sqlite3_exec(db, sql_script.c_str(), callback, 0, &error_msg);
         rapidjson::StringBuffer buffer;
         rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
         doc_json.Accept(writer);
+        sqlite3_close(db);
         //output with changes
         //std::cout << buffer.GetString() << std::endl;
+        /////////////////////////////////////////////////////////
+        /// to buffer_file to sqlite3
     }
     return 0;
 }
